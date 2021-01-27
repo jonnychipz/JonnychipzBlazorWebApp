4b2749ae9f4e56244e297fb6eae22a954a03e2f1
Jonnychipz <john@luinn-ltd.co.uk>
2021-01-27 12:40:07 +0000

    comit

--- client/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ client/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,84 +13,84 @@ namespace JonnychipzBlazorWebApp.Client.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\Projects\BlazorDemo\JonnychipzBlazorWebApp\client\_Imports.razor"
+#line 1 "d:\Projects\BlazorDemo\JonnychipzBlazorWebApp\client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\Projects\BlazorDemo\JonnychipzBlazorWebApp\client\_Imports.razor"
+#line 2 "d:\Projects\BlazorDemo\JonnychipzBlazorWebApp\client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\Projects\BlazorDemo\JonnychipzBlazorWebApp\client\_Imports.razor"
+#line 3 "d:\Projects\BlazorDemo\JonnychipzBlazorWebApp\client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\Projects\BlazorDemo\JonnychipzBlazorWebApp\client\_Imports.razor"
+#line 4 "d:\Projects\BlazorDemo\JonnychipzBlazorWebApp\client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\Projects\BlazorDemo\JonnychipzBlazorWebApp\client\_Imports.razor"
+#line 5 "d:\Projects\BlazorDemo\JonnychipzBlazorWebApp\client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\Projects\BlazorDemo\JonnychipzBlazorWebApp\client\_Imports.razor"
+#line 6 "d:\Projects\BlazorDemo\JonnychipzBlazorWebApp\client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\Projects\BlazorDemo\JonnychipzBlazorWebApp\client\_Imports.razor"
+#line 7 "d:\Projects\BlazorDemo\JonnychipzBlazorWebApp\client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\Projects\BlazorDemo\JonnychipzBlazorWebApp\client\_Imports.razor"
+#line 8 "d:\Projects\BlazorDemo\JonnychipzBlazorWebApp\client\_Imports.razor"
 using JonnychipzBlazorWebApp.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\Projects\BlazorDemo\JonnychipzBlazorWebApp\client\_Imports.razor"
+#line 9 "d:\Projects\BlazorDemo\JonnychipzBlazorWebApp\client\_Imports.razor"
 using JonnychipzBlazorWebApp.Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "D:\Projects\BlazorDemo\JonnychipzBlazorWebApp\client\_Imports.razor"
+#line 12 "d:\Projects\BlazorDemo\JonnychipzBlazorWebApp\client\_Imports.razor"
 using MatBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\Projects\BlazorDemo\JonnychipzBlazorWebApp\client\Shared\MainLayout.razor"
+#line 3 "d:\Projects\BlazorDemo\JonnychipzBlazorWebApp\client\Shared\MainLayout.razor"
 using Radzen;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\Projects\BlazorDemo\JonnychipzBlazorWebApp\client\Shared\MainLayout.razor"
+#line 4 "d:\Projects\BlazorDemo\JonnychipzBlazorWebApp\client\Shared\MainLayout.razor"
 using Radzen.Blazor;
 
 #line default
